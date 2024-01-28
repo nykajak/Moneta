b7150dcf80206518958adaadc784c4cd27da7205
Nykaj A K <2nykajak@gmail.com>
2024-01-28 10:53:27 +0530

    Continuation of previous commit.

--- moneta/models.py
+++ moneta/models.py
@@ -4,6 +4,12 @@ from flask_login import UserMixin
 # from flask_security import UserMixin,RoleMixin
 # from flask_security.models import fsqla_v3 as fsqla
 
+# Table to store written relation between Book and Author
+written = db.Table('written',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
+    db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True)
+)
+
 # Table to store category relation between Book and Section
 category = db.Table('category',
     db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
@@ -67,10 +73,9 @@ class Book(db.Model):
 
     # Fields
     id = db.Column(db.Integer,primary_key = True)
-    author_id = db.Column(db.Integer,db.ForeignKey("author.id"),nullable = False)
     name = db.Column(db.String(80), unique = True, nullable = False)
-    content = db.Column(db.Text,nullable = False)
 
+    # Reference to all ratings assigned to the book
     ratings = db.relationship('User',secondary=rating)
 
     def __repr__(self):
@@ -82,6 +87,9 @@ class Author(db.Model):
     #Fields
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(80), unique = True, nullable = False)
+
+    # Reference to all ratings written by author and vice versa.
+    books = db.relationship('Book',secondary=written,backref = db.backref('authors',lazy='dynamic') )
 
     def __repr__(self):
         return f'Author({self.name})'
@@ -95,6 +103,7 @@ class Section(db.Model):
     doc = db.Column(db.DateTime, default = date.today())
     description = db.Column(db.String(256))
 
+    # Reference to all books in section and book.sections to find all sections book belongs to.
     books = db.relationship('Book',secondary=category,backref = db.backref('sections',lazy='dynamic') )
 
     def __repr__(self):
