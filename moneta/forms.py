ce05a8f3712df07500f9ccbe1e548097d0e86543
Nykaj A K <2nykajak@gmail.com>
2024-03-14 09:28:57 +0530

    Removed unused import in forms

--- moneta/forms.py
+++ moneta/forms.py
@@ -1,6 +1,6 @@
 # Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,RadioField,FileField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,RadioField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 # User Forms
