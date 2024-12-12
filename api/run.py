4eefb6031e989d31b8c746ef81e9d18e379a5345
Nykaj <2nykajak@gmail.com>
2024-12-12 14:43:20 +0530

    Commented out db.createall

--- api/run.py
+++ api/run.py
@@ -2,8 +2,8 @@ from api import app,db
 import api.models 
 
 
-with app.app_context():
-    db.create_all()
-    app.logger.info("Database initialised!")
+# with app.app_context():
+#     db.create_all()
+#     app.logger.info("Database initialised!")
         
 # app.run()
