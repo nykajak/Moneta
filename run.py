d7e0d86f38936e3352a6ca458e776595ea0c33c8
Nykaj <2nykajak@gmail.com>
2024-12-12 14:10:37 +0530

    Removed app.run and added vercel.json

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from moneta import app,db
 import moneta.models 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        app.logger.info("Database initialised!")
+
+with app.app_context():
+    db.create_all()
+    app.logger.info("Database initialised!")
         
-    app.run()
+# app.run()
